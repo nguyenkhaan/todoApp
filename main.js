7991f68697892c45bb11349e6e4a04faba687d21
nguyenkhaan <nguyenkhaan2006@gmail.com>
2025-02-19 15:01:10 +0700

    v2

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const app = document.querySelector('.app');
 const todoHeading = document.querySelector('.todo__heading'); 
 const todoHeadingInput = document.querySelector('.todo__heading__input'); 
 const todoHeadingAdd = document.querySelector('.todo__heading__add'); 
-     
+
 var todo = document.querySelector('.todo'); 
 function add(input) 
 {
@@ -10,10 +10,17 @@ function add(input)
     todoList.classList.add('todo__list'); 
     todoList.innerHTML = 
         `
-            <span class="todo__list__info">
-                    ${input}
-            </span>
-            <i class="fa-regular fa-trash-can"></i>
+            <div class="todo__info__wrap">
+                    <span class="todo__list__info">
+                        ${input}
+                    </span>
+                </div>
+                <div class="todo__list__icon">
+                    <i class="fa-solid fa-circle icon__status icon--red" style = "color: #B82132;"></i>
+                    <i class="fa-solid fa-circle icon__status icon--yellow" style = "color: #FFB22C;"></i>
+                    <i class="fa-solid fa-circle icon__status icon--green" style = "color: #5CB338"></i>
+                    <i class="fa-regular fa-trash-can todo__list__rubbish"></i>
+                </div>
         `
     todo.appendChild(todoList); 
 }
@@ -24,22 +31,70 @@ function handleTask()
         add(task); 
         todoHeadingInput.value = ''; 
         todoHeadingInput.focus(); 
-        var todoList = document.querySelectorAll('.todo__list');
-        for (let i of todoList) 
+        var todoList = document.querySelector('.todo__list:last-of-type'); 
+        var spanWrap = todoList.querySelector('.todo__info__wrap');  
+        var iconWrap = todoList.querySelector('.todo__list__icon'); 
+        const iconWrapItem = iconWrap.querySelectorAll('i'); 
+        //Module Uncomlete Modifier 
         {
-            i.addEventListener('click' , () => {
-                if (i.classList.contains('uncomplete')) {
-                    i.classList.remove('uncomplete'); 
-                } 
-                else i.classList.add('uncomplete'); 
+            spanWrap.addEventListener('click' , () => {
+                
+                spanWrap.classList.toggle('uncomplete'); 
+                if (!spanWrap.classList.contains('uncomplete')) {
+                    todoList.classList.remove('icon--red', 'icon--yellow' , 'icon--green');
+                }
+                for (let i of iconWrapItem) {
+                    if (spanWrap.classList.contains('uncomplete')) i.classList.add('lost--color'); 
+                    else i.classList.remove('lost--color'); 
+                }
             })
-            var removeButton = i.querySelector('i'); 
+             var removeButton = todoList.querySelector('.todo__list__rubbish'); 
             removeButton.addEventListener('click' ,() => {
-                todo.removeChild(i); 
+                todo.removeChild(todoList); 
             })
         }
+        //Module Color classify 
+        {
+            /**Task1 */
+            const subItem = [] 
+            const subColor = [] 
+            for (let i = 0; i < iconWrapItem.length - 1; ++i)
+            {
+                const item = iconWrapItem[i]; 
+                const classifyName = item.className.split(' ').pop(); 
+                subItem.push(item); 
+                subColor.push(classifyName); 
+            }
+            /**Task2 */
+            for (let i = 0; i < subItem.length; ++i) 
+            {
+                const removeSubClass = () => {
+                    todoList.classList.remove('icon--red', 'icon--yellow' , 'icon--green');
+                }     
+                subItem[i].addEventListener('click' , () => {  
+                    for (let j = 0; j < subColor.length; ++j) 
+                    {
+                        if (j == i) subItem[j].classList.toggle('lost--color'); 
+                        else subItem[j].classList.remove('lost--color'); 
+                    }
+                    removeSubClass(); 
+                    if (i == 0) {
+                        if (subItem[i].classList.contains('lost--color')) todoList.classList.add('icon--red');
+                        else todo.classList.remove('icon--red'); 
+                    }
+                    else if (i == 1) {
+                        if (subItem[i].classList.contains('lost--color')) todoList.classList.add('icon--yellow');
+                        else todo.classList.remove('icon--yellow'); 
+                    }
+                    else {
+                        if (subItem[i].classList.contains('lost--color')) todoList.classList.add('icon--green');
+                        else todo.classList.remove('icon--green'); 
+                    }
+                })
+            }
+        }
     }
-    else alert('Task trong, khong the them'); 
+    else alert('Task trống, không thể thêm'); 
 }
 function mainUI() 
 {
